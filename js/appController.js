88d3e0dcaa7930adab943ed1ad2c1544b9e5829e
Saurabh Sharma <saursharma@google.com>
2014-12-31 20:01:36 +0530

    Enabling angular material and adding basic components

--- js/appController.js
+++ js/appController.js
@@ -1,5 +1,7 @@
-var websiteBuilder = angular.module('websiteBuilder',[]);
+var websiteBuilder = angular.module('websiteBuilder',['ngMaterial']);
 
-websiteBuilder.controller('websiteCtrl', function($scope)) {
-  $scope.name = 'saurabh';
-}
+websiteBuilder.controller('websiteCtrl', function($scope) {
+  $scope.name = 'Saurabh Sharma';
+
+  $scope.quote = 'the people who are crazy enough to think they can change the world are the ones who do ~  Steve Jobs';
+});
