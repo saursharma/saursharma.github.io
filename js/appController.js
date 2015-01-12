3f0f7d55e41becc963da1ddfded262a1fb4a5a52
Saurabh <saurabh@imaniac.org>
2015-01-12 23:57:52 +0530

    Enabling multiple tabs. Adding bio card.

--- js/appController.js
+++ js/appController.js
@@ -1,11 +1,13 @@
 var websiteBuilder = angular.module('websiteBuilder',['ngMaterial']);
 
 websiteBuilder.controller('websiteCtrl', function($scope, $interval) {
-  $scope.name = 'Saurabh Sharma';
+  $scope.name = 'Saurabh Sharma';  
+    
+  $scope.bio = 'FullStack developer, in love with JavaScript. Working on client side at Google, Gurgaon sice more than 2 years. Have worked mainly on AngularJS, PolymerJS and Python.';
 
   $scope.quote = 'the people who are crazy enough to think they can change the world are the ones who do ~  Steve Jobs';
 
-  //$scope.js = 0;
+  $scope.details = '23 years old, Male, living in Gurgaon';
 
   $scope.skills = [{
     name: 'JavaScript',
@@ -25,20 +27,9 @@ websiteBuilder.controller('websiteCtrl', function($scope, $interval) {
     score: 50
   }];
 
-  // Need to find optimized way to create these function.
- // for (i=0; i<$scope.skills.length; i++) {
-   // window.console.log($scope.skills[i].counter);
-    $interval(function() {
-      if($scope.skills[0].counter < $scope.skills[0].score) $scope.skills[0].counter += 1;
-    }, 20, 0, true);
- // }
-    $interval(function() {
-      if($scope.skills[1].counter < $scope.skills[1].score) $scope.skills[1].counter += 1;
-    }, 20, 0, true);
-    $interval(function() {
-      if($scope.skills[2].counter < $scope.skills[2].score) $scope.skills[2].counter += 1;
-    }, 20, 0, true);
-    $interval(function() {
-      if($scope.skills[3].counter < $scope.skills[3].score) $scope.skills[3].counter += 1;
-    }, 20, 0, true);
+  $interval(function() {
+    for (i=0; i<$scope.skills.length; i++) {
+      if($scope.skills[i].counter < $scope.skills[i].score) $scope.skills[i].counter += 1;
+    }
+  }, 20, 0, true);
 });
