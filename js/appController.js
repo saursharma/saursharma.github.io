02c3a18b2c0fdd3fffd153993f5612be8327dd11
Saurabh <saurabh@imaniac.org>
2015-01-22 00:44:55 +0530

    Adding Bottom Sheet

--- js/appController.js
+++ js/appController.js
@@ -1,6 +1,6 @@
 var websiteBuilder = angular.module('websiteBuilder',['ngMaterial']);
 
-websiteBuilder.controller('websiteCtrl', function($scope, $interval) {
+websiteBuilder.controller('websiteCtrl', function($scope, $interval, $mdBottomSheet) {
   $scope.name = 'Saurabh Sharma';  
     
   $scope.bio = 'FullStack developer, in love with JavaScript. Working on client side at Google, Gurgaon sice more than 2 years. Have worked mainly on AngularJS, PolymerJS and Python.';
@@ -27,9 +27,29 @@ websiteBuilder.controller('websiteCtrl', function($scope, $interval) {
     score: 50
   }];
 
+  $scope.showBottomSheet = function($event) {
+    $mdBottomSheet.show({
+        templateUrl: 'bottom-sheet.html',
+        controller: 'BottomSheetCtrl',
+        targetEvent: $event
+    });
+  };
+
   $interval(function() {
     for (i=0; i<$scope.skills.length; i++) {
       if($scope.skills[i].counter < $scope.skills[i].score) $scope.skills[i].counter += 1;
     }
   }, 20, 0, true);
+});
+
+websiteBuilder.controller('BottomSheetCtrl', function($scope) {
+  $scope.social = [{
+    website: 'LinkedIn',
+    profile: 'https://www.linkedin.com/profile/view?id=74513702',
+    icon: 'linked in'
+  }, {
+    website: 'Facebook',
+    profile: 'https://www.facebook.com/sharma.saurabh114',
+    icon: 'facebook'
+  }];
 });
